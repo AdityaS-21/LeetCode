962d3c6f7452c87f9621755eb44e64a125796d35
Aditya Srivastav <114082319+AdityaS-21@users.noreply.github.com>
2024-03-03 01:05:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (26.06%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
-    int cal(vector<int>& nums,int ind,vector<int>& dp){
-        if(ind==0) return nums[0];
-        if(ind<0) return 0;
-        if(dp[ind]!=-1) return dp[ind];
-        int pick = nums[ind] + cal(nums,ind-2,dp);
-        int Not_pick = 0 + cal(nums,ind-1,dp);
-        return dp[ind]=max(pick,Not_pick);
-    }
+    //--------------------MEMOIZATION---------------------
+    // int cal(vector<int>& nums,int ind,vector<int>& dp){
+    //     if(ind==0) return nums[0];
+    //     if(ind<0) return 0;
+    //     if(dp[ind]!=-1) return dp[ind];
+    //     int pick = nums[ind] + cal(nums,ind-2,dp);
+    //     int Not_pick = 0 + cal(nums,ind-1,dp);
+    //     return dp[ind]=max(pick,Not_pick);
+    // }
     int rob(vector<int>& nums) {
+    //----------Tabulation-------------
         int n=nums.size();
         vector<int>dp(n,-1);
-        return cal(nums,n-1,dp);   
+        dp[0]=nums[0];
+        for(int i=1;i<n;i++){
+            int pick = nums[i];
+            if(i>1) pick+=dp[i-2];
+            int Not_pick = 0 + dp[i-1];
+            dp[i]=max(pick,Not_pick);
+        }  
+        return dp[n-1];
     }
 };
