390e6212d915b8c961eb89e93ef5f0767d271ea4
Aditya Srivastav <114082319+AdityaS-21@users.noreply.github.com>
2024-07-02 17:26:36 +0530

    Time: 17 ms (73.26%) | Memory: 24.6 MB (85.72%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -4,7 +4,9 @@ public:
         int l =0,h=nums.size()-1;
         while(l<h){
             int mid  = (h-l)/2 + l;
-            if(mid%2 && nums[mid]==nums[mid-1] || !(mid%2) && nums[mid]==nums[mid+1] ){
+            // if(mid%2 && nums[mid]==nums[mid-1] || !(mid%2) && nums[mid]==nums[mid+1] )
+            //-----------------or---------------------- 
+            if(nums[mid]==nums[mid^1]){ //in this if mid is odd then mid^1 will be mid-1 and if mid is even mid^1 will be mid+1
                 l=mid+1;
             }
             else{
