a11f437ed458fd2b1e1c0a862a0b12b25a559e4d
Aditya Srivastav <114082319+AdityaS-21@users.noreply.github.com>
2024-07-15 14:42:47 +0530

    Time: 62 ms (77.28%) | Memory: 45.4 MB (48.14%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -5,8 +5,8 @@ public:
       int sum =0;
       int count =0;
       mp[0]++;
-      for(auto i:nums){
-        sum+=i;
+      for(int i=0;i<nums.size();i++){
+        sum+=nums[i];
         if(mp.find(sum-k)!=mp.end()) count+=mp[sum-k];
         mp[sum]++;
       }
